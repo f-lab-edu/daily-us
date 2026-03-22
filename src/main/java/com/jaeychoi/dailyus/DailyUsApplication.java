5f1e6cd3575ba43e4dcef5d1a667c228e21551df
j-yong98 <chlwodyd9829@gmail.com>
2026-03-23 01:11:04 +0900

    chore: Spring Boot 프로젝트 추가

---
+++ src/main/java/com/jaeychoi/dailyus/DailyUsApplication.java
@@ -0,0 +1,13 @@
+package com.jaeychoi.dailyus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyUsApplication.class, args);
+	}
+
+}
