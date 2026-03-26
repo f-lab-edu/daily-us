6c55080bc350141b2546daa90b96ceaedd816524
j-yong98 <chlwodyd9829@gmail.com>
2026-03-27 00:18:01 +0900

    feat: JWT Filter 구현

--- src/main/java/com/jaeychoi/dailyus/common/exception/ErrorCode.java
+++ src/main/java/com/jaeychoi/dailyus/common/exception/ErrorCode.java
@@ -5,7 +5,11 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
   EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "USR_001", "Email is already in use."),
   NICKNAME_ALREADY_EXISTS(HttpStatus.CONFLICT, "USR_002", "Nickname is already in use."),
+  INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "AUTH_001", "Email or password is invalid."),
+  UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "AUTH_002", "Authentication is required."),
   INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_003", "Token is invalid or expired."),
+  FORBIDDEN(HttpStatus.FORBIDDEN, "AUTH_004",
+      "You do not have permission to access this resource."),
   INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_005",
       "Refresh token is invalid or expired.");
 
