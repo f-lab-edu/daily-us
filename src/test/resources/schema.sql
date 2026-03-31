ae6ee64f64d13dcb9f934972a21df9c90b571304
j-yong98 <chlwodyd9829@gmail.com>
2026-04-01 01:16:41 +0900

    chore: 그룹 관련 test schema 추가

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -13,3 +13,28 @@ CREATE TABLE users (
     CONSTRAINT uk_users_email UNIQUE (email),
     CONSTRAINT uk_users_nickname UNIQUE (nickname)
 );
+
+CREATE TABLE user_groups (
+    group_id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    intro VARCHAR(500) NULL,
+    group_image VARCHAR(500) NULL,
+    owner_id BIGINT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP NULL,
+    PRIMARY KEY (group_id),
+    CONSTRAINT fk_groups_owner_id
+        FOREIGN KEY (owner_id) REFERENCES users (user_id)
+);
+
+CREATE TABLE group_members (
+    group_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (group_id, user_id),
+    CONSTRAINT fk_group_members_group_id
+        FOREIGN KEY (group_id) REFERENCES user_groups (group_id),
+    CONSTRAINT fk_group_members_user_id
+        FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
