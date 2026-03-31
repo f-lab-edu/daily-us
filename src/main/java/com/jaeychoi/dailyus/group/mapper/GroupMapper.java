50cea834c7625d43058fd04b038c4e54fb8b2be6
j-yong98 <chlwodyd9829@gmail.com>
2026-04-01 00:47:42 +0900

    feat: 그룹 생성 Mapper 추가

---
+++ src/main/java/com/jaeychoi/dailyus/group/mapper/GroupMapper.java
@@ -0,0 +1,12 @@
+package com.jaeychoi.dailyus.group.mapper;
+
+import com.jaeychoi.dailyus.group.domain.Group;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface GroupMapper {
+
+  void insert(Group group);
+
+  void insertMember(Long groupId, Long userId);
+}
