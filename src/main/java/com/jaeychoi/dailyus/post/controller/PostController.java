1d95017c105b8e41b1617541868d03d266e240a0
j-yong98 <chlwodyd9829@gmail.com>
2026-04-06 15:27:10 +0900

    feat: 피드 목록 엔드 포인트 구현

--- src/main/java/com/jaeychoi/dailyus/post/controller/PostController.java
+++ src/main/java/com/jaeychoi/dailyus/post/controller/PostController.java
@@ -6,13 +6,17 @@ import com.jaeychoi.dailyus.auth.domain.CurrentUser;
 import com.jaeychoi.dailyus.common.web.ApiResponse;
 import com.jaeychoi.dailyus.post.dto.PostCreateRequest;
 import com.jaeychoi.dailyus.post.dto.PostCreateResponse;
+import com.jaeychoi.dailyus.post.dto.PostFeedResponse;
 import com.jaeychoi.dailyus.post.service.PostCreateService;
+import com.jaeychoi.dailyus.post.service.PostFeedService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +26,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
 
   private final PostCreateService postCreateService;
+  private final PostFeedService postFeedService;
+
+  @GetMapping
+  @AuthRequired
+  public ApiResponse<PostFeedResponse> getFeed(@AuthenticatedUser CurrentUser user,
+      @RequestParam(required = false, defaultValue = "0") Long page,
+      @RequestParam(required = false, defaultValue = "10") Long size) {
+    return ApiResponse.success(postFeedService.getFeed(user.userId(), page, size));
+  }
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
