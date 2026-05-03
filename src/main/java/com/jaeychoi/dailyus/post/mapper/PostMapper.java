11e201fe4e07d554338d92a962079d00fc131f2d
j-yong98 <chlwodyd9829@gmail.com>
2026-05-03 23:52:03 +0900

    feat: 본인 게시물 조회 매퍼 추가

--- src/main/java/com/jaeychoi/dailyus/post/mapper/PostMapper.java
+++ src/main/java/com/jaeychoi/dailyus/post/mapper/PostMapper.java
@@ -20,5 +20,7 @@ public interface PostMapper {
 
   List<PostFeedRow> findRecentFeedPosts(Long size, LocalDateTime createdAt, Long postId);
 
+  List<PostFeedRow> findPostsByUserId(Long userId, Long size, LocalDateTime createdAt, Long postId);
+
   List<PostImageRow> findImagesByPostIds(List<Long> postIds);
 }
