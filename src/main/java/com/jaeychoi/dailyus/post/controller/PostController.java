b9d1b3663534cc5094663abeb216a0b22274c498
j-yong98 <chlwodyd9829@gmail.com>
2026-05-05 18:55:04 +0900

    feat: 댓글 조회 컨트롤러 추가

--- src/main/java/com/jaeychoi/dailyus/post/controller/PostController.java
+++ src/main/java/com/jaeychoi/dailyus/post/controller/PostController.java
@@ -3,6 +3,8 @@ package com.jaeychoi.dailyus.post.controller;
 import com.jaeychoi.dailyus.auth.annotation.AuthRequired;
 import com.jaeychoi.dailyus.auth.annotation.AuthenticatedUser;
 import com.jaeychoi.dailyus.auth.domain.CurrentUser;
+import com.jaeychoi.dailyus.comment.dto.CommentResponse;
+import com.jaeychoi.dailyus.comment.service.CommentGetService;
 import com.jaeychoi.dailyus.common.web.ApiResponse;
 import com.jaeychoi.dailyus.post.dto.PostCreateRequest;
 import com.jaeychoi.dailyus.post.dto.PostCreateResponse;
@@ -15,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +32,7 @@ public class PostController {
 
   private final PostCreateService postCreateService;
   private final PostFeedService postFeedService;
+  private final CommentGetService commentGetService;
 
   @GetMapping
   @AuthRequired
@@ -39,6 +43,22 @@ public class PostController {
       @RequestParam(required = false) Long postId,
       @RequestParam(required = false, defaultValue = "10") Long size) {
     return ApiResponse.success(postFeedService.getFeed(user.userId(), createdAt, postId, size));
+  }
+
+  @GetMapping("/{postId}/comments")
+  @AuthRequired
+  public ApiResponse<CommentResponse> getComments(
+      @PathVariable Long postId,
+      @AuthenticatedUser CurrentUser user,
+      @RequestParam(required = false)
+      @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+      LocalDateTime createdAt,
+      @RequestParam(required = false) Long commentId,
+      @RequestParam(required = false, defaultValue = "10") Long size
+  ) {
+    return ApiResponse.success(
+        commentGetService.getComments(postId, user.userId(), createdAt, commentId, size)
+    );
   }
 
   @PostMapping
