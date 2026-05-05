83298f7aaa2fa8b777b5588387ee13cdd8017494
j-yong98 <chlwodyd9829@gmail.com>
2026-05-05 19:08:40 +0900

    feat: 그룹 목록 조회 컨트롤러 추가

--- src/main/java/com/jaeychoi/dailyus/group/controller/GroupController.java
+++ src/main/java/com/jaeychoi/dailyus/group/controller/GroupController.java
@@ -7,15 +7,21 @@ import com.jaeychoi.dailyus.common.web.ApiResponse;
 import com.jaeychoi.dailyus.group.dto.GroupCreateRequest;
 import com.jaeychoi.dailyus.group.dto.GroupCreateResponse;
 import com.jaeychoi.dailyus.group.dto.GroupJoinResponse;
+import com.jaeychoi.dailyus.group.dto.GroupListResponse;
 import com.jaeychoi.dailyus.group.service.GroupCreateService;
 import com.jaeychoi.dailyus.group.service.GroupJoinService;
+import com.jaeychoi.dailyus.group.service.GroupListService;
 import jakarta.validation.Valid;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +32,19 @@ public class GroupController {
 
   private final GroupCreateService groupCreateService;
   private final GroupJoinService groupJoinService;
+  private final GroupListService groupListService;
+
+  @GetMapping
+  @AuthRequired
+  public ApiResponse<GroupListResponse> getGroups(
+      @RequestParam(required = false)
+      @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+      LocalDateTime createdAt,
+      @RequestParam(required = false) Long groupId,
+      @RequestParam(required = false, defaultValue = "10") Long size
+  ) {
+    return ApiResponse.success(groupListService.getGroups(createdAt, groupId, size));
+  }
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
@@ -40,7 +59,8 @@ public class GroupController {
 
   @PostMapping("/{groupId}/join")
   @AuthRequired
-  public ApiResponse<GroupJoinResponse> joinGroup(@PathVariable Long groupId, @AuthenticatedUser CurrentUser user) {
+  public ApiResponse<GroupJoinResponse> joinGroup(@PathVariable Long groupId,
+      @AuthenticatedUser CurrentUser user) {
     GroupJoinResponse response = groupJoinService.join(groupId, user.userId());
     return ApiResponse.success(response);
   }
