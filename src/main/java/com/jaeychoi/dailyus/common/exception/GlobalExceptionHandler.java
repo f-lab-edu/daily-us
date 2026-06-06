12b949749ffa6a949caea05154a266ec45e7fdc0
j-yong98 <chlwodyd9829@gmail.com>
2026-06-06 13:56:20 +0900

    chore: Slf4j 에노테이션 변경

--- src/main/java/com/jaeychoi/dailyus/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jaeychoi/dailyus/common/exception/GlobalExceptionHandler.java
@@ -1,18 +1,17 @@
 package com.jaeychoi.dailyus.common.exception;
 
 import com.jaeychoi.dailyus.common.web.ApiResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-  private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
   @ExceptionHandler(BaseException.class)
   public ResponseEntity<ApiResponse<Void>> handleBaseException(BaseException e) {
--- src/main/java/com/jaeychoi/dailyus/post/scheduler/PostLikeCountFlushScheduler.java
+++ src/main/java/com/jaeychoi/dailyus/post/scheduler/PostLikeCountFlushScheduler.java
@@ -5,8 +5,6 @@ import com.jaeychoi.dailyus.post.repository.PostLikeRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -16,7 +14,6 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class PostLikeCountFlushScheduler {
 
-  private static final Logger log = LoggerFactory.getLogger(PostLikeCountFlushScheduler.class);
   private final PostLikeRepository postLikeRepository;
   private final PostMapper postMapper;
 
